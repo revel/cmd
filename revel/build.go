af269f69a2197e4d04ead629c6ac2a8c983cf363
verdverm <verdverm@gmail.com>
2014-02-25 23:23:47 -0500

    hackaroo for packaging

--- revel/build.go
+++ revel/build.go
@@ -100,13 +100,13 @@ func buildApp(args []string) {
 
 	mustRenderTemplate(
 		runShPath,
-		path.Join(revel.RevelPath, "revel", "package_run.sh.template"),
+		path.Join(revel.RevelPath, "../cmd/revel", "package_run.sh.template"),
 		tmplData)
 
 	mustChmod(runShPath, 0755)
 
 	mustRenderTemplate(
 		path.Join(destPath, "run.bat"),
-		path.Join(revel.RevelPath, "revel", "package_run.bat.template"),
+		path.Join(revel.RevelPath, "../cmd/revel", "package_run.bat.template"),
 		tmplData)
 }

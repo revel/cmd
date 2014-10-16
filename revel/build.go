6ea9d159b2fbc8eeaef540bca5b99ba48483b363
Justin Li <jli.justinli@gmail.com>
2014-10-16 10:48:24 -0400

    Split up hardcoded path separators

--- revel/build.go
+++ revel/build.go
@@ -100,13 +100,13 @@ func buildApp(args []string) {
 
 	mustRenderTemplate(
 		runShPath,
-		filepath.Join(revel.RevelPath, "../cmd/revel", "package_run.sh.template"),
+		filepath.Join(revel.RevelPath, "..", "cmd", "revel", "package_run.sh.template"),
 		tmplData)
 
 	mustChmod(runShPath, 0755)
 
 	mustRenderTemplate(
 		filepath.Join(destPath, "run.bat"),
-		filepath.Join(revel.RevelPath, "../cmd/revel", "package_run.bat.template"),
+		filepath.Join(revel.RevelPath, "..", "cmd", "revel", "package_run.bat.template"),
 		tmplData)
 }

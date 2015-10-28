71e0d776735a26be033341171146d52a003c1a2b
runner.mei <runner.mei@gmail.com>
2015-10-28 18:30:24 +0800

    add mixed template engine support

--- revel/build.go
+++ revel/build.go
@@ -75,7 +75,7 @@ func buildApp(args []string) {
 	mustCopyFile(destBinaryPath, app.BinaryPath)
 	mustChmod(destBinaryPath, 0755)
 	mustCopyDir(path.Join(tmpRevelPath, "conf"), path.Join(revel.RevelPath, "conf"), nil)
-	if revel.GO_TEMPLATE == revel.Config.StringDefault(revel.REVEL_TEMPLATE_ENGINE, revel.GO_TEMPLATE) {
+	if tmpl := revel.Config.StringDefault(revel.REVEL_TEMPLATE_ENGINE, revel.GO_TEMPLATE); revel.GO_TEMPLATE == tmpl || revel.MIXED_TEMPLATE == tmpl {
 		mustCopyDir(path.Join(tmpRevelPath, "templates"), path.Join(revel.RevelPath, "templates"), nil)
 	}
 	mustCopyDir(path.Join(srcPath, filepath.FromSlash(appImportPath)), revel.BasePath, nil)

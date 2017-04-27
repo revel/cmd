e1776bda3c1f45b2d0315ca7b40d443d5f7578e1
NotZippy <notzippy@gmail.com>
2017-04-26 21:04:12 -0700

    Rollback a change that was committed by mistake to develop branch

--- harness/harness.go
+++ harness/harness.go
@@ -47,10 +47,7 @@ type Harness struct {
 	proxy      *httputil.ReverseProxy
 }
 
-func renderError(iw http.ResponseWriter, ir *http.Request, err error) {
-    r,w := &revel.GORequest{Goheader:&revel.GOHeader{}}, &revel.GOResponse{Goheader:&revel.GOHeader{}}
-    r.Set(ir)
-    w.Set(iw)
+func renderError(w http.ResponseWriter, r *http.Request, err error) {
 	req, resp := revel.NewRequest(r), revel.NewResponse(w)
 	c := revel.NewController(req, resp)
 	c.RenderError(err).Apply(req, resp)
